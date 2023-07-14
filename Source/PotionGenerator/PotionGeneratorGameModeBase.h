9bfd674fae00c909650100f7a8a6a68f89fe525a
Dillon Drummond <dillondrum70@gmail.com>
2023-07-14 19:11:22 -0400

    Adding initial project

---
+++ Source/PotionGenerator/PotionGeneratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PotionGeneratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POTIONGENERATOR_API APotionGeneratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
