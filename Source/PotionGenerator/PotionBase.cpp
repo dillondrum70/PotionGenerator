5e33f580e6425e612101e5e7114c2edafdea4ec1
Dillon Drummond <dillondrum70@gmail.com>
2023-07-19 23:46:16 -0400

    Adding wobble effect based on the potion's position last frame

--- Source/PotionGenerator/PotionBase.cpp
+++ Source/PotionGenerator/PotionBase.cpp
@@ -3,6 +3,12 @@
 
 #include "PotionBase.h"
 #include "Components/StaticMeshComponent.h"
+#include "Materials/Material.h"
+#include "Components/SceneComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/MovementComponent.h"
+#include "Engine/World.h"
+#include "Math/UnrealMathUtility.h"
 
 // Sets default values
 APotionBase::APotionBase()
@@ -10,10 +16,10 @@ APotionBase::APotionBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	bottleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bottle Mesh"));
-	bottleMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-	liquidMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Liquid Mesh"));
-	liquidMesh->AttachToComponent(bottleMesh, FAttachmentTransformRules::KeepRelativeTransform);
+	BottleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bottle Mesh"));
+	BottleMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	LiquidMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Liquid Mesh"));
+	LiquidMesh->AttachToComponent(BottleMesh, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 // Called when the game starts or when spawned
@@ -21,6 +27,10 @@ void APotionBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	matInst = UMaterialInstanceDynamic::Create(LiquidMesh->GetMaterial(0), this);
+	LiquidMesh->SetMaterial(0, matInst);
+
+	lastPos = GetActorLocation();
 }
 
 // Called every frame
@@ -28,5 +38,33 @@ void APotionBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	WobbleX = FMath::Lerp(WobbleX, 0, DeltaTime * WobbleSpeed);
+	WobbleY = FMath::Lerp(WobbleY, 0, DeltaTime * WobbleSpeed);
+
+	float pulse = 2 * PI * WobbleSpeed;
+	TargetWobbleX = WobbleX * FMath::Sin(pulse * GetWorld()->GetTime().GetRealTimeSeconds());
+	TargetWobbleY = WobbleY * FMath::Sin(pulse * GetWorld()->GetTime().GetRealTimeSeconds());
+
+	FVector vel = lastPos - GetActorLocation();
+
+	WobbleX = FMath::Clamp(WobbleX + (-vel.Y * WobbleLimit), -WobbleLimit, WobbleLimit);
+	WobbleY = FMath::Clamp(WobbleY + (vel.X * WobbleLimit), -WobbleLimit, WobbleLimit);
+
+	matInst->SetScalarParameterValue(WobbleXName, TargetWobbleX);
+	matInst->SetScalarParameterValue(WobbleYName, TargetWobbleY);
+
+
+	/*FVector vel = lastPos - GetActorLocation();
+	TargetWobbleX = -vel.Y;
+	TargetWobbleY = vel.X;
+
+	WobbleX = FMath::Clamp(FMath::Lerp(WobbleX, TargetWobbleX, WobbleSpeed * DeltaTime), -WobbleLimit, WobbleLimit);
+	WobbleY = FMath::Clamp(FMath::Lerp(WobbleY, TargetWobbleY, WobbleSpeed * DeltaTime), -WobbleLimit, WobbleLimit);
+		
+	*/
+
+	lastPos = GetActorLocation();
+
+	UE_LOG(LogTemp, Display, TEXT("%f  %f"), WobbleX, WobbleY);
 }
 
--- Source/PotionGenerator/PotionBase.h
+++ Source/PotionGenerator/PotionBase.h
@@ -7,6 +7,9 @@
 #include "PotionBase.generated.h"
 
 class UStaticMeshComponent;
+class UMaterial;
+class UMaterialInstanceDynamic;
+class UMovementComponent;
 
 UCLASS()
 class POTIONGENERATOR_API APotionBase : public AActor
@@ -15,10 +18,29 @@ class POTIONGENERATOR_API APotionBase : public AActor
 
 private:
 	UPROPERTY(EditAnywhere, Category = Model, meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UStaticMeshComponent> bottleMesh;
-	
+	TObjectPtr<UStaticMeshComponent> BottleMesh;
 	UPROPERTY(EditAnywhere, Category = Model, meta = (AllowPrivateAccess = "true"))
-	TObjectPtr<UStaticMeshComponent> liquidMesh;
+	TObjectPtr<UStaticMeshComponent> LiquidMesh;
+
+	UPROPERTY(EditAnywhere, Category = Material, meta = (AllowPrivateAccess = "true"))
+	FName WobbleXName = TEXT("WobbleX");
+	UPROPERTY(EditAnywhere, Category = Material, meta = (AllowPrivateAccess = "true"))
+	FName WobbleYName = TEXT("WobbleY");
+
+	TObjectPtr<UMaterialInstanceDynamic> matInst;
+
+	float WobbleX = 0;
+	float TargetWobbleX = 0;
+
+	float WobbleY = 0;
+	float TargetWobbleY = 0;
+
+	FVector lastPos;
+
+	UPROPERTY(EditAnywhere, Category = Potion, meta = (AllowPrivateAccess = "true"))
+	float WobbleSpeed = 3;
+	UPROPERTY(EditAnywhere, Category = Potion, meta = (AllowPrivateAccess = "true"))
+	float WobbleLimit = .75f;
 	
 public:	
 	// Sets default values for this actor's properties
